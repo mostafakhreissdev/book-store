76585bb6eada9475b1434a168f0c874799bff27b
mostafa khreiss <kobracodee>
2025-11-24 15:18:31 +0200

    moved footer css inside index.css

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,17 +1,9 @@
 import React from 'react';
+import '../index.css';
 
 function Footer() {
   return (
-    <footer  style={{
-        background: "#222",
-        color: "white",
-        textAlign: "center",
-        padding: "15px",
-        position: "fixed",
-        bottom: 0,
-        left: 0,
-        width: "100%"
-      }}>
+    <footer>
       &copy; {new Date().getFullYear()} MagicBook onlinestore. All rights reserved.
     </footer>
   );
