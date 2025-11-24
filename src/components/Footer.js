2c376c6088c6bd038b404094768ebed3a5c15c97
mostafa khreiss <kobracodee>
2025-11-24 14:55:10 +0200

    fixed footer position to fixed

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,16 @@ import React from 'react';
 
 function Footer() {
   return (
-    <footer>
+    <footer  style={{
+        background: "#222",
+        color: "white",
+        textAlign: "center",
+        padding: "15px",
+        position: "fixed",
+        bottom: 0,
+        left: 0,
+        width: "100%"
+      }}>
       &copy; {new Date().getFullYear()} MagicBook onlinestore. All rights reserved.
     </footer>
   );
